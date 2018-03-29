6605a76e81ab7d40bc01f20c7cd3d3ff0162e2fa
rodilon <renanodilon@gmail.com>
2018-03-29 19:08:47 -0300

    Aplicação para aprendizado do framework rails 4

---
+++ app/controllers/produtos_controller.rb
@@ -0,0 +1,64 @@
+class ProdutosController < ApplicationController
+    
+    before_action :set_produto, only: [:edit, :update, :destroy]
+    
+    def index
+        @produtos_por_nome = Produto.order(:nome).limit 5
+        @produtos_por_preco = Produto.order(:preco).limit 2
+    end
+    def new 
+        @produto = Produto.new
+        @departamentos = Departamento.all
+        renderiza :new
+    end
+    
+    def busca
+        @nome_a_buscar = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome_a_buscar}%"
+    end
+
+    def edit
+        renderiza :edit
+    end
+
+    def update
+        if @produto.update produto_params
+            flash[:notice] = "Produto atualizado com sucesso"
+            redirect_to root_path
+        else
+            rederiza :edit
+        end
+    end
+
+    def create
+        @produto = Produto.new produto_params
+        if @produto.save
+            flash[:notice] = "Produto salvo com sucesso"
+            redirect_to root_path
+        else
+            renderiza :new
+        end
+    end
+
+    def destroy
+        @produto.destroy
+        redirect_to root_url
+    end
+
+    private
+
+    def renderiza(view)
+        @departamentos = Departamento.all
+        render view
+    end
+
+    def set_produto
+        id = params[:id]
+        @produto = Produto.find(id)
+    end
+
+    def produto_params
+        params.require(:produto).permit :nome, :preco, :descricao, 
+                                                    :quantidade, :departamento_id
+    end
+end
